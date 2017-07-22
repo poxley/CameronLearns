40aa5e6fae1dcb452007c53453a69bb40930b2d6
poxley <poxley@nnu.edu>
2017-07-22 15:33:17 -0600

    fixed serialization
    toObject now working as expected

--- scripts/tasks-controller.js
+++ scripts/tasks-controller.js
@@ -1,27 +1,30 @@
 tasksController = function() {
    var taskPage;
-   var intialised = false;
+   var initialised = false;
    return {
       init : function(page) {
          if (!initialised) {
             taskPage = page;
-            $(taskPage.find( '[required="required"]' ).prev('label').append( '<spand>*</span>').children( 'span').addClass('required');
-            $(taskPage.find).find('tbody tr:even').addClass('even');
-            $(taskPage.find( '#btnAddTask').click( function(evt) {
+            $(taskPage).find('[required="required"]').prev('label').append('<span>*</span>').children('span').addClass('required');
+            $(taskPage).find('tbody tr:even').addClass('even');
+            $(taskPage).find('#btnAddTask').click(function(evt) {
                evt.preventDefault();
-               $(taskPage ).find('#taskCreation' ).removeClass('not');
+               $(taskPage).find('#taskCreation').removeClass('not');
             });
-            $(taskPage).find('tbody tr' ).click(function(evt) {
-               $(evt.target ).closest('td').siblings( ).addBack().toggleClass('rowHighlight');
+            $(taskPage).find('tbody tr').click(function(evt) {
+               $(evt.target).closest('td').siblings().addBack().toggleClass('rowHighlight');
             });
-            $(taskPage).find('#tblTasks tbody').on('click', 'deleteRow', function(evt) {
+            $(taskPage).find('#tblTasks tbody').on('click', '.deleteRow', function(evt) {
                evt.preventDefault();
-               #(evt.target ).parents('tr').remove();
+               $(evt.target).parents('tr').remove();
             });
             $(taskPage).find('#saveTask').click(function(evt) {
                evt.preventDefault();
-               var task = $('from').toObject();
-               $('#taskRow').tmpl(task).appendTo($(taskPage).find('#tblTasks tbody'));
+//               if ($(taskPage).find('form').valid()) {
+                  var task = $('form').toObject();
+//                  var task = $(taskPage).find('task').val();
+                  $('#taskRow').tmpl(task).appendTo($(taskPage).find('#tblTasks tbody'));
+//               }
             });
             initialised = true;
          }
